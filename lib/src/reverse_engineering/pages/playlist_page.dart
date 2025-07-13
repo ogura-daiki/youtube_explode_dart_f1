4a883afb1d2d7b7a63e24160f667850ba30eaca8
OGURA DAIKI <dkog2001@gmail.com>
2025-07-13 21:12:05 +0900

    nullチェックでエラーが出ていたのでnullを許可するように戻した

--- lib/src/reverse_engineering/pages/playlist_page.dart
+++ lib/src/reverse_engineering/pages/playlist_page.dart
@@ -282,7 +282,7 @@ class _Video {
 
   String? get _videoInfo => root
       .get('videoInfo')
-      ?.getT<List<dynamic>>('runs')!
-      .cast<Map<dynamic, dynamic>>()
+      ?.getT<List<dynamic>>('runs')
+      ?.cast<Map<dynamic, dynamic>>()
       .parseRuns();
 }
